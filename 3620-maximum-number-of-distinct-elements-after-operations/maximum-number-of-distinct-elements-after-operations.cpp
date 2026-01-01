2120ba78892b09ccf1c586340bd9edbef1fde1ff
Shruti <169065066+yadavshruti6@users.noreply.github.com>
2026-01-01 22:00:56 +0530

    Time: 105 ms (91.69%) | Memory: 97.7 MB (33.25%) - LeetSync

---
+++ 3620-maximum-number-of-distinct-elements-after-operations/maximum-number-of-distinct-elements-after-operations.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxDistinctElements(vector<int>& nums, int k) {
+        if (nums.size() <= 2L * k + 1) {
+            return nums.size();
+        }
+        
+        sort(nums.begin(), nums.end());
+        long long current = LLONG_MIN;
+        int res = 0;
+        
+        for (int num : nums) {
+            long long target = max(current + 1, (long long)num - k);
+            
+            if (target <= num + k) {
+                current = target;
+                res++;
+            } else {
+                current = max(current, (long long)num);
+            }
+        }
+        
+        return res;
+    }
+};
